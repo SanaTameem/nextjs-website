0449039d8bd26754b7c082f72c725441afd0107a
unknown <sanatameem38@gmail.com>
2024-11-01 22:14:03 +0430

    Add error.jsx file

---
+++ src/app/error.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+
+function error() {
+  return (
+    <div className='continer my-5 py-5 d-flex justify-content-center align-items-center flex-column'>
+      <h1>Error</h1>
+      <p>Something went wrong...</p>
+    </div>
+  )
+}
+
+export default error
